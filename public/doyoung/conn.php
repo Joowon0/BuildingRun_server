95577c6a57f4d8fd509dad1c5d96d9561f1a1506
Joowon <joowon7508@gmail.com>
2018-07-26 23:35:35 -0700

    historical moment

--- public/doyoung/conn.php
+++ public/doyoung/conn.php
@@ -1,5 +1,5 @@
 ﻿<?php
 mysql_connect('127.0.0.1','teama-iot','a3fgss90tf239') or die("Unable to connect");
-mysql_select_db("team-2018summer") or die("Unable to select database");
+mysql_select_db("teama-2018summer") or die("Unable to select database");
 mysql_query("set names utf8");
 ?>
