958283312e7377790cdcb42f63bc04d6cbce4cfd
gkehdud <41028214+gkehdud@users.noreply.github.com>
2018-07-27 03:53:12 -0700

    Update updatePass.php

--- public/doyoung/updatePass.php
+++ public/doyoung/updatePass.php
@@ -1,15 +1,14 @@
-﻿<?php
+<?php
 require "conn.php";
-$a_dong=isset($_POST['a_dong']) ? $_POST['a_dong'] : '';  
-$a_ho=isset($_POST['a_ho']) ? $_POST['a_ho'] : '';
-$a_password=isset($_POST['a_password']) ? $_POST['a_password'] : '';
-$mysql_qry ="update post set a_password='$a_password' where a_dong LIKE '%$a_dong%' and a_ho LIKE '%$a_ho%'";
+$user_email=isset($_POST['EmailAddress']) ? $_POST['EmailAddress'] : '';
+$user_pass=isset($_POST['HPassword']) ? $_POST['HPassword'] : '';
+$mysql_qry ="update User set HPassword='$user_pass' where EmailAddress = '$user_email'";
 $result = mysqli_query($conn, $mysql_qry);
 if($result){  
-       echo "password change success";  
+       echo 1;  
     }  
     else{  
-       echo "password change failed"; 
+       echo 0; 
        echo mysqli_error($conn);
     } 
    
@@ -27,9 +26,8 @@ if (!$android){
    <body>
    
       <form action="<?php $_PHP_SELF ?>" method="POST">
-         a_dong: <input type = "text" name = "a_dong" />
-         a_ho: <input type = "text" name = "a_ho" />
-       a_password: <input type = "text" name="a_password" />
+         Email: <input type = "text" name = "user_email" />
+              a_password: <input type = "text" name="user_pass" />
          <input type = "submit" />
       </form>
    
