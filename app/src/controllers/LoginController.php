f55d75fec440cc80e15ec8d0c854e8dbe842536b
Joowon <joowon7508@gmail.com>
2018-07-30 05:22:31 +0000

    login successgit add -A!

--- app/src/controllers/LoginController.php
+++ app/src/controllers/LoginController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-
-
 final class LoginController extends BaseController {
   //abstract class loginMessage {
-      const SUCCESS = 0;
-      const NO_SUCH_EMAIL = 1;
-      const WRONG_PASSWORD = 2;
+      const SUCCESS = 1;
+      const NO_SUCH_EMAIL = 2;
+      const WRONG_PASSWORD = 3;
   //}
 
   function checkPassword($email, $password) {
@@ -34,25 +32,10 @@ final class LoginController extends BaseController {
   // TODO : need to check if email and password entry is not null
   function loginHandler(Request $request, Response $response, $args) {
     $pwCheckResult = $this->checkPassword($_POST['email'], $_POST['password']);
-
-    switch($pwCheckResult) {
-      case self::NO_SUCH_EMAIL:
-        $message = "There is no account corresponding to input Email";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-        break;
-      case self::WRONG_PASSWORD:
-        $message = "Wrong Password. Please enter again.";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-        break;
-      case self::SUCCESS:
-        $message = "Wrong Password. Please enter again.";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-        break;
-      default :
-        $message = "ERROR : smth wrong in checkPassword().";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-    }
-
+    if ($pwCheckResult == self::SUCCESS)
+      $this->view->render($response, 'main_after.phtml',  ['email' => $_POST['email'], 'firstName' => $firstName, 'lastName' => $lastName]);
+    else
+      $this->view->render($response, 'login.phtml', ['emailResult'=>$pwCheckResult]);
   	// TODO : need to check if activated
   }
 }
