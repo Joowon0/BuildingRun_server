bb0887dac5156e620b8b0e904f7247cfa5c7450d
gkehdud <41028214+gkehdud@users.noreply.github.com>
2018-07-29 22:09:36 -0700

    Update findpw.php

--- public/doyoung/findpw.php
+++ public/doyoung/findpw.php
@@ -2,48 +2,13 @@
 require "conn.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PhPMailer\Exception;
+
 if($_POST["EmailAddress"]!=""){
 $EmailAddress=$_POST["EmailAddress"];
 $FirstName=$_POST["FirstName"];
 $LastName=$_POST["LastName"];
-$mysql_qry ="select * from user where EmailAddress = '".$EmailAddress."'";
 $result = mysqli_query($conn, $mysql_qry);
-if(mysql_num_rows($result) > 0 )
-{
-    while($data = mysql_fetch_array($result)){
-         	   $FirstName=$data["FirstName"];
-		   $LastName=$data["LastName"];
-                   sendEmail($EmailAddress,$FirstName,$LastName);
-	}
-}
-else 
-{
-   echo 0;
-}
-}
-mysqli_close($conn);
-?>
-<?php
-$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
-if (!$android){
-?>
-
-<html>
-   <body>
-   
-      <form action="<?php $_PHP_SELF ?>" method="POST">
-         Email: <input type = "text" name = "EmailAddress" />
-              firstName: <input type = "text" name="FirstName" />
-         	  lastName: <input type = "text" name="LastName" />
-         
-	<input type = "submit" />
-      </form>
-   
-   </body>
-</html>
-<?php
-}
-public function sendEmail($email, $firstName, $lastName) {
+	public function sendEmail($email, $firstName, $lastName) {
   $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
   try {
     //Server settings
@@ -70,5 +35,30 @@ public function sendEmail($email, $firstName, $lastName) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
   }
+}
+
+sendEmail($EmailAddress,$FirstName,$LastName);
+}
+mysqli_close($conn);
+?>
+<?php
+$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+if (!$android){
+?>
+
+<html>
+   <body>
+   
+      <form action="<?php $_PHP_SELF ?>" method="POST">
+         Email: <input type = "text" name = "EmailAddress" />
+              firstName: <input type = "text" name="FirstName" />
+         	  lastName: <input type = "text" name="LastName" />
+         
+	<input type = "submit" />
+      </form>
+   
+   </body>
+</html>
+<?php
 }
 ?>
