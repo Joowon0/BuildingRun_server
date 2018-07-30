34215b33071d98c25a09ac645d4fc6aa4ed4a84a
gkehdud <41028214+gkehdud@users.noreply.github.com>
2018-07-29 19:55:23 -0700

    Update checkEmail.php

--- public/doyoung/checkEmail.php
+++ public/doyoung/checkEmail.php
@@ -2,7 +2,7 @@
 require "conn.php";
 if($_POST["EmailAddress"]!=""){
 $EmailAddress=$_POST["EmailAddress"];
-$qry = "select * from User where EmailAddress = '$EmailAddress'";
+$qry = "select * from User where EmailAddress = '".$EmailAddress."'";
 $result = mysql_query($qry);
 if(mysql_num_rows($result) > 0 )
    {
