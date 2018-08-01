cdfcc5f311af3b0c2dca4d10f035a83f77dc3134
Joowon <joowon7508@gmail.com>
2018-07-31 22:10:40 -0700

    while doing forgotten password

--- app/src/controllers/forgottenPWController.php
+++ app/src/controllers/forgottenPWController.php
@@ -4,9 +4,6 @@ namespace App\Controller;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 final class forgottenPWController extends BaseController {
   public function forgot_pw(Request $request, Response $response, $args)
   {
@@ -16,6 +13,7 @@ final class forgottenPWController extends BaseController {
 
   public function forgot_pwHandler(Request $request, Response $response, $args)
   {
+      
       $this->view->render($response, 'forgot_pw.phtml');
       return $response;
   }
