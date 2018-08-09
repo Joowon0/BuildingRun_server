ee99ad4f4cc7203c86aeb9a77426e5075f2bedf4
Joowon <joowon7508@gmail.com>
2018-08-09 13:50:57 -0700

    show table backward

--- app/src/controllers/ChartController.php
+++ app/src/controllers/ChartController.php
@@ -27,6 +27,7 @@ final class ChartController extends BaseController {
   }
   public function heartRateReal(Request $request, Response $response, $args) {
     $sql = "SELECT SUBSTR(Timestamp, 12, 2) as hh, SUBSTR(Timestamp, 15, 2) as mm, SUBSTR(Timestamp, 18, 2) as ss, HeartRate, HeartInterval FROM Heart_Info WHERE USN = ".$_SESSION["USN"]." ORDER BY Timestamp DESC LIMIT 20";
+    $sqlShow = "SELECT * FROM " . $sql ." ORDER BY hh, mm, ss";
     // echo $_SESSION["USN"];
     // echo $sql;
     // exit;
