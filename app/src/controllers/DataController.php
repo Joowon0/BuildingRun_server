ce22189ceb958a62630a4824fedc6adc419e2bcb
Joowon <joowon7508@gmail.com>
2018-08-09 02:12:35 -0700

    air quality transfer done

--- app/src/controllers/DataController.php
+++ app/src/controllers/DataController.php
@@ -5,17 +5,25 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 final class DataController extends BaseController {
+  //abstract class loginMessage {
+      const MAC_EXIST = 1;
+      const MAC_NOT_EXIST = 2;
+  //}
+
   // for APP
   public function app_airQualityDataTransfer(Request $request, Response $response, $args) {
     $json = file_get_contents('php://input');
     $jsonArray = json_decode($json, true);
 
-    if (isset($jsonArray['USN']) && isset($jsonArray['TIME']) && isset($jsonArray['SSN'])) {
+    if (isset($jsonArray['USN']) && isset($jsonArray['TIME']) && isset($jsonArray['MAC'])) {
 
-      //$this->storeGPS($jsonArray);
-      $this->storeAirQuality($jsonArray);
+      $this->storeGPS($jsonArray);
 
-      $sendData = array("ACK"=>true);
+      $MAC_existence = $this->checkMAC($jsonArray['MAC']);
+      if ($MAC_existence == self::MAC_EXIST)
+        $this->storeAirQuality($jsonArray);
+
+      $sendData = array("Result"=>$MAC_existence);
 
       return $response->withStatus(200)
           ->withHeader('Content-Type', 'application/json')
@@ -27,8 +35,7 @@ final class DataController extends BaseController {
   }
 
   public function storeGPS($data) {
-    $sql = "INSERT INTO GPS(Out_lat, Out_log, Timestamp, USN) VALUES (".
-      $data['latitude']. ", ". $data['longitude'] .", '" . $data['TIME'] . "', " . $data['USN']. ")" ;
+    $sql = "UPDATE Sensor SET latitude = ".$data['latitude'].", longitude=".$data['longitude']." WHERE USN = ". $data['USN'] ;
     try {
       $stmt = $this->db->query($sql);
       return true;
@@ -37,17 +44,34 @@ final class DataController extends BaseController {
     }
   }
 
+  public function checkMAC($MAC) {
+    $sql = "SELECT * FROM Sensor WHERE MAC = '" .$MAC. "'" ;
+    try {
+      $stmt = $this->db->query($sql);
+      $result = $stmt->fetch();
+
+      if ($result == null) {
+        return self::MAC_NOT_EXIST;
+      } else {
+        return self::MAC_EXIST;
+      }
+    } catch (PDOException $e) {
+      echo "ERROR : " . $e->getMessage();
+    }
+  }
+
   // TODO : need to calcul AQI
   public function storeAirQuality($data) {
-    $sql = "INSERT INTO AirQuality_Info(CO, CO_AQI, SO2, SO2_AQI, NO2, NO2_AQI, O3, O3_AQI, PM2_5, PM2_5_AQI, AQI_avg, Temp, SSN, Timestamp) VALUES (".
-      $data['CO']    . ", -1, ".
-      $data['SO2']   . ", -1, " .
-      $data['NO2']   . ", -1, ".
-      $data['O3']    . ", -1, ".
-      $data['PM2.5'] . ", -1, -1,".
-      $data['temper']. ",".
-      $data['SSN']   . ", '".
-      $data['TIME']."' )" ;
+    $sql = "INSERT INTO AirQuality_Info(MAC, Timestamp, CO, SO2, NO2, O3, PM25, TEMP) VALUES ('".
+      $data['MAC']."', '".
+      $data['TIME']."', ".
+      $data['CO'].", ".
+      $data['SO2'].", ".
+      $data['NO2'].", ".
+      $data['O3'].", ".
+      $data['PM25'].", ".
+      $data['TEMP']." ) " ;
+      // echo $sql; exit;
     try {
       $stmt = $this->db->query($sql);
       return true;
