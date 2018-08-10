dc7d25f478d4b352fdb950ac56143253823002f0
Joowon <joowon7508@gmail.com>
2018-08-10 15:29:29 +0000

    all cases done

--- app/src/controllers/ChartController.php
+++ app/src/controllers/ChartController.php
@@ -88,7 +88,8 @@ final class ChartController extends BaseController {
         $airData = $this->minAirQuality($jsonArray['MAC'], $jsonArray['startDate'], $jsonArray['endDate']);
       else if ($jsonArray['period'] == 2)
         $airData = $this->hourAirQuality($jsonArray['MAC'], $jsonArray['startDate'], $jsonArray['endDate']);
-        // $airData = $this->dayAirQuality($jsonArray['MAC'], $jsonArray['startDate'], $jsonArray['endDate']);;
+      else if ($jsonArray['period'] == 3)
+        $airData = $this->dayAirQuality($jsonArray['MAC'], $jsonArray['startDate'], $jsonArray['endDate']);;
 
       return $response->withStatus(200)
           ->withHeader('Content-Type', 'application/json')
@@ -134,24 +135,24 @@ final class ChartController extends BaseController {
       echo "ERROR : " . $e->getMessage();
     }
   }
-  // public function dayAirQuality($MAC, $startDate, $endDate) {
-  //   $sql = "SELECT SUBSTR(Timestamp, 1, 10) as TIME, avg(CO) AS CO, avg(SO2) AS SO2, avg(NO2) AS NO2, avg(O3) AS O3, avg(PM25) AS PM25, avg(TEMP) as TEMP FROM AirQuality_Info WHERE '".$startDate."' <= Timestamp  AND Timestamp < '".$endDate."' GROUP BY SUBSTR(Timestamp, 1, 10)";
-  //   try {
-  //   	$stmt = $this->db->query($sql);
-  //   	$result = $stmt->fetch();
-  //
-  //     $airData = array();
-  //
-  //     while ($result != null) {
-  //       array_push($airData, $result);
-  //       $result = $stmt->fetch();
-  //     }
-  //     //print_r($data); exit;
-  //     return $airData;
-  //   } catch (PDOException $e) {
-  //     echo "ERROR : " . $e->getMessage();
-  //   }
-  // }
+  public function dayAirQuality($MAC, $startDate, $endDate) {
+    $sql = "SELECT SUBSTR(Timestamp, 1, 10) as TIME, avg(CO) AS CO, avg(SO2) AS SO2, avg(NO2) AS NO2, avg(O3) AS O3, avg(PM25) AS PM25, avg(TEMP) as TEMP FROM AirQuality_Info WHERE MAC = '".$MAC."' AND '".$startDate."' <= Timestamp  AND Timestamp < '".$endDate."' GROUP BY SUBSTR(Timestamp, 1, 10)";
+    try {
+    	$stmt = $this->db->query($sql);
+    	$result = $stmt->fetch();
+
+      $airData = array();
+
+      while ($result != null) {
+        array_push($airData, $result);
+        $result = $stmt->fetch();
+      }
+      //print_r($data); exit;
+      return $airData;
+    } catch (PDOException $e) {
+      echo "ERROR : " . $e->getMessage();
+    }
+  }
 
   // for APP historic data view
   public function app_heartHistory(Request $request, Response $response, $args) {
@@ -162,10 +163,11 @@ final class ChartController extends BaseController {
         isset($jsonArray['startDate']) && isset($jsonArray['endDate'])) {
 
       if ($jsonArray['period'] == 1)
-;
+        $airData = $this->minHeart($jsonArray['USN'], $jsonArray['startDate'], $jsonArray['endDate']);
       else if ($jsonArray['period'] == 2)
         $airData = $this->hourHeart($jsonArray['USN'], $jsonArray['startDate'], $jsonArray['endDate']);
-        // $airData = $this->dayHeart($jsonArray['USN'], $jsonArray['startDate'], $jsonArray['endDate']);;
+      else if ($jsonArray['period'] == 3)
+        $airData = $this->dayHeart($jsonArray['USN'], $jsonArray['startDate'], $jsonArray['endDate']);;
 
       return $response->withStatus(200)
           ->withHeader('Content-Type', 'application/json')
@@ -173,6 +175,28 @@ final class ChartController extends BaseController {
     }
     else
       return $response->withStatus(204);
+  }
+
+  public function minHeart($USN, $startDate, $endDate) {
+    $sql = "SELECT SUBSTR(Timestamp, 1, 16) as TIME, avg(HeartRate) as HeartRate, avg(HeartInterval) as HeartInterval
+            FROM Heart_Info
+            WHERE USN = ".$USN." AND
+            '".$startDate."' <= Timestamp  AND Timestamp < '".$endDate."' GROUP BY SUBSTR(Timestamp, 1, 16)";
+    try {
+    	$stmt = $this->db->query($sql);
+    	$result = $stmt->fetch();
+
+      $airData = array();
+
+      while ($result != null) {
+        array_push($airData, $result);
+        $result = $stmt->fetch();
+      }
+      //print_r($data); exit;
+      return $airData;
+    } catch (PDOException $e) {
+      echo "ERROR : " . $e->getMessage();
+    }
   }
 
   public function hourHeart($USN, $startDate, $endDate) {
@@ -197,26 +221,26 @@ final class ChartController extends BaseController {
     }
   }
 
-  // public function dayHeart($USN, $startDate, $endDate) {
-  //   $sql = "SELECT SUBSTR(Timestamp, 1, 10) as TIME, avg(HeartRate) as HeartRate, avg(HeartInterval) as HeartInterval
-  //           FROM Heart_Info
-  //           WHERE USN = ".$USN." AND
-  //           '".$startDate."' <= Timestamp  AND Timestamp < '".$endDate."' GROUP BY SUBSTR(Timestamp, 1, 10)";
-  //
-  //   try {
-  //   	$stmt = $this->db->query($sql);
-  //   	$result = $stmt->fetch();
-  //
-  //     $airData = array();
-  //
-  //     while ($result != null) {
-  //       array_push($airData, $result);
-  //       $result = $stmt->fetch();
-  //     }
-  //     //print_r($data); exit;
-  //     return $airData;
-  //   } catch (PDOException $e) {
-  //     echo "ERROR : " . $e->getMessage();
-  //   }
-  // }
+  public function dayHeart($USN, $startDate, $endDate) {
+    $sql = "SELECT SUBSTR(Timestamp, 1, 10) as TIME, avg(HeartRate) as HeartRate, avg(HeartInterval) as HeartInterval
+            FROM Heart_Info
+            WHERE USN = ".$USN." AND
+            '".$startDate."' <= Timestamp  AND Timestamp < '".$endDate."' GROUP BY SUBSTR(Timestamp, 1, 10)";
+
+    try {
+    	$stmt = $this->db->query($sql);
+    	$result = $stmt->fetch();
+
+      $airData = array();
+
+      while ($result != null) {
+        array_push($airData, $result);
+        $result = $stmt->fetch();
+      }
+      //print_r($data); exit;
+      return $airData;
+    } catch (PDOException $e) {
+      echo "ERROR : " . $e->getMessage();
+    }
+  }
 }
