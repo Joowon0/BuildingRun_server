b16dd169e9eafc50afb7578c907a23d8dff3bd51
Joowon <joowon7508@gmail.com>
2018-08-11 05:08:44 -0700

    sensor list

--- app/src/controllers/SensorListController.php
+++ app/src/controllers/SensorListController.php
@@ -5,88 +5,62 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 final class SensorListController extends BaseController {
 
-  public function sensor_json(Request $request, Response $response, $args) {
-    $sql = "SELECT Sensor.MAC as MAC, latitude, longitude, x.ts as Timestamp
-            FROM Sensor JOIN (SELECT MAC, max(Timestamp) ts FROM AirQuality_Info GROUP BY MAC) x
-            ON Sensor.MAC = x.MAC";
-    
-
-   try {
-            $sth = $this->db->query($sql);
-            $pogo = $sth->fetchAll();
-
-            if ($pogo) {
-                $sensor_array = [];
-                foreach ($pogo as $sensor) {
-
-                    $sensor_array[] = array(
-
-                        "MAC"=>$sensor['MAC'],
-                        "latitude" => $sensor['latitude'],
-                        "longitude" => $sensor['longitude'],
-                        "Timestamp" => $sensor['Timestamp'],
-                        
-                    );
-
-                }
-
-             
-                 return $response->withHeader('Content-Type', 'application/json')
-               ->write(json_encode($sensor_array, JSON_NUMERIC_CHECK))->withStatus(200);
-
-
-            } else {
-                $response = $response->withStatus(404);
-            }
-        } catch(PDOException $e) {
-            echo '{"error":{"text":'. $e->getMessage() .'}}';
-        }
-
-
-
-
-
-
-  }
+  // public function sensor_json(Request $request, Response $response, $args) {
+  //   $sql = "SELECT Sensor.MAC as MAC, latitude, longitude, x.ts as Timestamp
+  //           FROM Sensor JOIN (SELECT MAC, max(Timestamp) ts FROM AirQuality_Info GROUP BY MAC) x
+  //           ON Sensor.MAC = x.MAC";
+  //
+  //     try {
+  //       $sth = $this->db->query($sql);
+  //       $pogo = $sth->fetchAll();
+  //
+  //       if ($pogo) {
+  //         $sensor_array = [];
+  //         foreach ($pogo as $sensor) {
+  //
+  //           $sensor_array[] = array(
+  //             "MAC"=>$sensor['MAC'],
+  //             "latitude" => $sensor['latitude'],
+  //             "longitude" => $sensor['longitude'],
+  //             "Timestamp" => $sensor['Timestamp'],
+  //             );
+  //         }
+  //         return $response;
+  //       }
+  //     } catch(PDOException $e) {
+  //       echo '{"error":{"text":'. $e->getMessage() .'}}';
+  //     }
+  //     $this->view->render($response, 'sensor_list.phtml', ['sensor' => $sensor_array]);
+  //     return $response;
+  // }
 
   public function sensor_list(Request $request, Response $response, $args)
   {
-       $sql = "SELECT Sensor.MAC as MAC, latitude, longitude, x.ts as Timestamp
+    $sql = "SELECT Sensor.MAC as MAC, latitude, longitude, x.ts as Timestamp
             FROM Sensor JOIN (SELECT MAC, max(Timestamp) ts FROM AirQuality_Info GROUP BY MAC) x
             ON Sensor.MAC = x.MAC";
-    
 
-   try {
-            $sth = $this->db->query($sql);
-            $pogo = $sth->fetchAll();
+    try {
+      $sth = $this->db->query($sql);
+      $data = $sth->fetchAll();
 
-            if ($pogo) {
-                $sensor_array = [];
-                foreach ($pogo as $sensor) {
+      if ($data) {
+        $sensor_array = [];
+        foreach ($data as $sensor) {
 
-                    $sensor_array[] = array(
-
-                        "MAC"=>$sensor['MAC'],
-                        "latitude" => $sensor['latitude'],
-                        "longitude" => $sensor['longitude'],
-                        "Timestamp" => $sensor['Timestamp'],
-                        
-                    );
-
-                }
-
-             
-                 
-
-            } else {
-                $response = $response->withStatus(404);
-            }
-        } catch(PDOException $e) {
-            echo '{"error":{"text":'. $e->getMessage() .'}}';
+          $sensor_array[] = array(
+            "MAC"=>$sensor['MAC'],
+            "latitude" => $sensor['latitude'],
+            "longitude" => $sensor['longitude'],
+            "Timestamp" => $sensor['Timestamp'],
+            );
         }
+      }
+    } catch(PDOException $e) {
+      echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
 
-
-      $this->view->render($response, 'sensor_list.phtml', ['sensor' => $sensor_array]);
-      return $response;
+    $this->view->render($response, 'sensor_list.phtml', ['sensor' => $sensor_array]);
+    return $response;
   }
 }
